e7e924e73fc94df9a8f5af49106facf5ea4ac239
Patrick B Warren <patrickbwarren@gmail.com>
2023-07-05 14:05:30 +0100

    make refer to executable

--- crossgrad_simple.sh
+++ crossgrad_simple.sh
@@ -9,6 +9,6 @@ shape=005,0.125,0.025
 run_control="--njacobi=10000 --niter=40"
 grad=0.1,1
 
-echo crossgrad_simple.py $jobname --solve --nx=$size --ny=$size --shape=$shape --cx=$grad --cy=$grad $run_control
+echo ./crossgrad_simple.py $jobname --solve --nx=$size --ny=$size --shape=$shape --cx=$grad --cy=$grad $run_control
 
-echo crossgrad_viewer.py --rc ${jobname}.rc --show --phi-max=10.0 --detail --zoom 1.2
+echo ./crossgrad_viewer.py --rc ${jobname}.rc --show --phi-max=10.0 --detail --zoom 1.2
